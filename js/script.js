87aaa67b0571c6173f664f8570b48e5b020298ed
masud520 <masud.abdi@student.moringaschool.com>
2023-10-26 10:15:43 +0300

    completed assignment

--- js/script.js
+++ js/script.js
@@ -1,68 +1,64 @@
-document.addEventListener(`DOMContentLoaded`, () => {
-    const filmsList = document.getElementById('films');
-    const filmDetails = document.getElementById('film-details');
-    const buyTicketButton =document.getElementById('buy-ticket');
-
-    let currentFilm; // Store currently displayed film data
-    let availableTickets; // Number of available tickets
-
-    //fetch movie data from API endpoint
-    fetch('http://localhost:3000/films')
-      .then(response => response.json())
-      .then(films => {
-        //Load details of first movie when page loads
-        currentFilm = films[0];
-        availableTickets = currentFilm.capacity - currentFilm.tickets_sold;
-
-        //Display movie details
-        displayMovieDetails(currentFilm);
-
-        //Click event handler for "Buy Ticket" button
-        buyTicketButton.addEventListener('click', () => {
-            if(availableTickets > 0) {
-                //if tickets are available,decrement count and update display
-                availableTickets--;
-                currentFilm.tickets_sold++;
-                displayMovieDetails(currentFilm);
-
-                //Check if movie is sold out
-                if(availableTickets === 0) {
-                    buyTicketButton.textContent = 'Sold Out';
-                    buyTicketButton.classList.add('sold-out');
-                }
-            }
-        });
-
-        //Populate menu of all movies
-        films.forEach(film => {
-            const filmItem = document.createElement('li');
-            filmItem.textContent = film.title;
-            filmItem.className = 'film-item';
-            filmsList.appendChild(filmItem);
-
-            //Click event handler for movie selection
-            filmItem.addEventListener('click',() => {
-                currentFilm = film;
-                availableTickets = currentFilm.capacity - currentFilm.tickets_sold;
-                displayMovieDetails(currentFilm);
-
-                //Reset the "Buy Ticket" button when switching movies
-                buyTicketButton.textContent = 'Buy Ticket';
-                buyTicketButton.classList.remove('sold-out');
-            })
-            
-        });
-
-      })
-      .catch(error => console.error('Error fetching movie data:' , error));
-
-      //Helper function to display movie details
-      function displayMovieDetails(film) {
-        //Update DOM elements with movie details
-        document.getElementById('film-poster').src = film.poster;
-        document.getElementById('film-title').textContent = film.title;
-        document.getElementById('film-runtime').textContent = `Runtime: ${film.runtime} minutes`;
-        document.getElementById('film-showtime').textContent = `Showtime: ${film.showtime}`;
-        document.getElementById('film-tickets').textContent = `Available Tickets: ${availableTickets}`;
-      }
+// Execute the code when the DOM has fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    // Call the 'getMovies' function to fetch and display movie data
+    getMovies();
 });
+
+// Function to fetch movie data
+function getMovies() {
+    fetch("https://my-json-server.typicode.com/masud520/wk3_codechallenge/films")
+        .then(res => res.json())
+        .then(data => {
+            // Call the 'displayMovieDetails' function to display movie titles
+            displayMovieDetails(data);
+        });
+}
+
+// Function to display movie titles in a list
+function displayMovieDetails(data) {
+    for (let details of data) {
+        console.log(details);
+        // Get the 'films' list element
+        let list = document.getElementById("films");
+        // Create a list item element
+        let li = document.createElement('li');
+        li.textContent = details.title;
+        // Add a click event listener to display film details when clicked
+        li.addEventListener("click", () => displayFilmDetails(details));
+        // Append the list item to the 'films' list
+        list.appendChild(li);
+    }
+}
+
+// Function to display film details
+function displayFilmDetails(details) {
+    // Get the 'purchaseTickets' element
+    let purchaseTickets = document.getElementById("purchaseTickets");
+    // Populate the 'purchaseTickets' element with film details
+    purchaseTickets.innerHTML = `
+    <ul id="films">
+      <img src="${details.poster}">
+      <p>${details.title}</p>
+      <p>Description: ${details.description}</p>
+      <p>Runtime: ${details.runtime}</p>
+      <p>Showtime: ${details.showtime}</p>
+      <p id="me">available tickets: ${details.capacity - details.tickets_sold}
+      <button id="buyTicket">Buy Ticket</button>
+      <p id="soldOutMessage" style="color: brown;"></p>
+    </ul>
+    `;
+
+    // Get the 'buyTicket' button and 'availableTickets' element
+    let buyTicketButton = document.getElementById("buyTicket");
+    let availableTickets = document.getElementById("availableTickets");
+
+    // Add a click event listener to buy tickets
+    buyTicketButton.addEventListener("click", () => {
+        if (details.tickets_sold < details.capacity) {
+            details.tickets_sold++;
+            availableTickets.textContent = `Available tickets: ${details.capacity - details.tickets_sold}`;
+        } else if (details.tickets_sold === details.capacity) {
+            soldOutMessage.textContent = "Sold out!";
+        }
+    });
+}
